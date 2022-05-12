8b7bdbe07d9f5176de7b6e3d0a455e1fdf1a12ba
LuaBarbosa <lua.barbosa@yahoo.com.br>
2022-05-12 10:51:35 -0300

    Feat: criando projeto

---
+++ src/test/java/com/LetsCode/Productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LetsCode.Productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
