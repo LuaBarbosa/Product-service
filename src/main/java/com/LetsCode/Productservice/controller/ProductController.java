5359a003ec42d991f082d4399eaff845c7bbdb58
LuaBarbosa <lua.barbosa@yahoo.com.br>
2022-05-13 11:10:02 -0300

    Feat: incluindo serviço no controller

--- src/main/java/com/LetsCode/Productservice/controller/ProductController.java
+++ src/main/java/com/LetsCode/Productservice/controller/ProductController.java
@@ -1,7 +1,9 @@
 package com.LetsCode.Productservice.controller;
 
 import com.LetsCode.Productservice.Dto.ProductDto;
+import com.LetsCode.Productservice.service.ProductService;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,27 +13,34 @@ import java.util.List;
 @Log4j2
 public class ProductController {
 
+    private final ProductService service;
+
     @GetMapping(path = "/products")
-        public ResponseEntity<List<ProductDto>> getAllProductLIst(@PathVariable String Name){
+        public ResponseEntity<List<ProductDto>> getAllProductLIst(){
 
-        return null;
+        List<ProductDto> productList = service.getAllProductList();
+        return new ResponseEntity<>(productList, HttpStatus.OK);
+
     }
 
-    @DeleteMapping(path = "/products/products_id")
-    public ResponseEntity<List<ProductDto>> deleteProductLIst(@PathVariable String productName){
+    @DeleteMapping(path = "/products/{products_id}")
+    public ResponseEntity<List<ProductDto>> deleteProductLIst(@PathVariable(name = "productId") long productId){
 
-        return null;
+        service.deleteProductLIst(productId);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
-    @PutMapping(path = "/products/products_id")
-    public ResponseEntity<List<ProductDto>> putProductLIst(@PathVariable String productName){
+    @PutMapping(path = "/products")
+    public ResponseEntity<List<ProductDto>> updateProductLIst(@RequestBody ProductDto productDto){
 
-        return null;
+        service.updateProductList(productDto);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @PostMapping(path = "/products" )
-    public ResponseEntity<List<ProductDto>> postProductLIst(@PathVariable String productName){
+    public ResponseEntity<List<ProductDto>> postProduct(@RequestBody ProductDto productDto){
 
-        return null;
+        service.newProduct(productDto);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 }
