d41a334a55b829280a88e19d4ffe160aba874f51
LuaBarbosa <lua.barbosa@yahoo.com.br>
2022-05-13 11:20:29 -0300

    Feat: implementando a interface

--- src/main/java/com/LetsCode/Productservice/service/ProductServiceImpl.java
+++ src/main/java/com/LetsCode/Productservice/service/ProductServiceImpl.java
@@ -1,4 +1,27 @@
 package com.LetsCode.Productservice.service;
 
-public class ProductServiceImpl {
+import com.LetsCode.Productservice.Dto.ProductDto;
+
+import java.util.List;
+
+public class ProductServiceImpl  implements ProductService{
+
+    @Override
+    public List<ProductDto> getAllProductList() {
+        return null;
+    }
+    @Override
+    public void deleteProductLIst(long productId) {
+
+    }
+    @Override
+    public void updateProductList(ProductDto productDto) {
+
+    }
+
+    @Override
+    public void newProduct(ProductDto productDto) {
+
+    }
+
 }
