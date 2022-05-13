b378a94550f26bdb75b21cb51717a22b868f0db5
LuaBarbosa <lua.barbosa@yahoo.com.br>
2022-05-13 10:36:33 -0300

    Feat: criando Dto e importações

--- src/main/java/com/LetsCode/Productservice/Dto/ProductDto.java
+++ src/main/java/com/LetsCode/Productservice/Dto/ProductDto.java
@@ -1,4 +1,19 @@
 package com.LetsCode.Productservice.Dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class ProductDto {
+
+    private Long id;
+    private String Name;
+    private String Description;
+    private int Price;
+
+
 }
--- src/main/java/com/LetsCode/Productservice/controller/ProductController.java
+++ src/main/java/com/LetsCode/Productservice/controller/ProductController.java
@@ -3,9 +3,7 @@ package com.LetsCode.Productservice.controller;
 import com.LetsCode.Productservice.Dto.ProductDto;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -13,20 +11,27 @@ import java.util.List;
 @Log4j2
 public class ProductController {
 
-    @GetMapping("/products")
-    public ResponseEntity<List<ProductDto>> getAllProductLIst(@PathVariable(name = "products") String productName){
+    @GetMapping(path = "/products")
+        public ResponseEntity<List<ProductDto>> getAllProductLIst(@PathVariable String Name){
 
+        return null;
     }
 
+    @DeleteMapping(path = "/products/products_id")
     public ResponseEntity<List<ProductDto>> deleteProductLIst(@PathVariable String productName){
 
+        return null;
     }
 
+    @PutMapping(path = "/products/products_id")
     public ResponseEntity<List<ProductDto>> putProductLIst(@PathVariable String productName){
 
+        return null;
     }
 
+    @PostMapping(path = "/products" )
     public ResponseEntity<List<ProductDto>> postProductLIst(@PathVariable String productName){
 
+        return null;
     }
 }
